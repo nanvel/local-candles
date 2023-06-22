7b8f26100790d25f580a3f9175cf252e538b32eb
Oleksandr Polieno <polyenoom@gmail.com>
2023-06-22 23:51:17 +0700

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# Local candles
+
+Todo:
+- copy tests
+- only ohlcv
+- docs, usage examples
+- LOCAL_CANDLES_STORAGE=... env
+- option to pass source
---
+++ local_candles/__init__.py
@@ -0,0 +1,25 @@
+from typing import Union
+from pathlib import Path
+
+from .data import Data
+from .models import Time
+from .sources import Source, SOURCES
+
+
+__all__ = ("load_candles",)
+
+
+def load_candles(source: Union[Source, str], start_ts: Union[Time, str], stop_ts: Union[Time, str],
+                 fields=None, **kwargs):
+    start_ts = Time.from_string(start_ts)
+    stop_ts = Time.from_string(stop_ts)
+
+    if not isinstance(source, Source):
+        source = SOURCES[source](**kwargs)
+
+    return Data(cache_root=Path()).load_df(
+        source=source,
+        start_ts=start_ts,
+        stop_ts=stop_ts,
+        fields=fields or ["ts", "open", "high", "low", "close", "volume"],
+    )
